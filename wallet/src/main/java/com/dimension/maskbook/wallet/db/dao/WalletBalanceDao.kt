7ac5f15a25ceb3a2342ee07d9dc01d649b78e859
seiko <605590140@qq.com>
2022-01-10 13:52:05 +0800

    delete local wallet completely

--- wallet/src/main/java/com/dimension/maskbook/wallet/db/dao/WalletBalanceDao.kt
+++ wallet/src/main/java/com/dimension/maskbook/wallet/db/dao/WalletBalanceDao.kt
@@ -3,10 +3,13 @@ package com.dimension.maskbook.wallet.db.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.dimension.maskbook.wallet.db.model.DbWalletBalance
 
 @Dao
 interface WalletBalanceDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun add(data: List<DbWalletBalance>)
+    @Query("DELETE FROM DbWalletBalance WHERE walletId = :walletId")
+    suspend fun deleteByWalletId(walletId: String)
 }
--- wallet/src/main/java/com/dimension/maskbook/wallet/db/dao/WalletTokenDao.kt
+++ wallet/src/main/java/com/dimension/maskbook/wallet/db/dao/WalletTokenDao.kt
@@ -3,10 +3,13 @@ package com.dimension.maskbook.wallet.db.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.dimension.maskbook.wallet.db.model.DbWalletToken
 
 @Dao
 interface WalletTokenDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun add(data: List<DbWalletToken>)
+    @Query("DELETE FROM DbWalletToken WHERE walletId = :walletId")
+    suspend fun deleteByWalletId(walletId: String)
 }
