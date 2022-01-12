42242a8565ae36bb8d783ca8cbe30ee5576d4a2d
seiko <605590140@qq.com>
2022-01-12 16:23:41 +0800

    add isDarkTheme param in MaskTheme

--- wallet/src/main/java/com/dimension/maskbook/wallet/ui/Theme.kt
+++ wallet/src/main/java/com/dimension/maskbook/wallet/ui/Theme.kt
@@ -2,7 +2,12 @@ package com.dimension.maskbook.wallet.ui
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.Typography
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
@@ -14,92 +19,93 @@ import org.koin.androidx.compose.get
 
 @Composable
 fun MaskTheme(
+    isDarkTheme: Boolean = isDarkTheme(),
     content: @Composable () -> Unit,
 ) {
     MaterialTheme(
-        colors = provideColor(),
+        colors = provideColor(isDarkTheme),
         shapes = provideShapes(),
-        typography = provideTypography(),
+        typography = provideTypography(isDarkTheme),
         content = content,
     )
 }
 
 @Composable
-fun provideTypography(): Typography {
+fun provideTypography(isDarkTheme: Boolean): Typography {
     return Typography().let {
         it.copy(
             body1 = it.body1.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color(0XCCFFFFFF)
                 } else {
                     Color(0XFF6B738D)
                 },
             ),
             body2 = it.body2.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color(0XCCFFFFFF)
                 } else {
                     Color(0XFF6B738D)
                 },
             ),
             h1 = it.h1.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color(0XCCFFFFFF)
                 } else {
                     Color(0XFF1D2238)
                 }
             ),
             h2 = it.h2.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color(0XCCFFFFFF)
                 } else {
                     Color(0XFF1D2238)
                 }
             ),
             h3 = it.h3.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color(0XCCFFFFFF)
                 } else {
                     Color(0XFF1D2238)
                 }
             ),
             h4 = it.h4.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color(0XCCFFFFFF)
                 } else {
                     Color(0XFF1D2238)
                 }
             ),
             h5 = it.h5.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color(0XCCFFFFFF)
                 } else {
                     Color(0XFF1D2238)
                 }
             ),
             h6 = it.h6.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color(0XCCFFFFFF)
                 } else {
                     Color(0XFF1D2238)
                 }
             ),
             subtitle1 = it.subtitle1.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color(0XCCFFFFFF)
                 } else {
                     Color(0XFF1D2238)
                 }
             ),
             subtitle2 = it.subtitle2.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color(0XCCFFFFFF)
                 } else {
                     Color(0XFF1D2238)
                 }
             ),
             button = it.button.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color.Unspecified
 //                    Color(0XCCFFFFFF)
                 } else {
@@ -108,14 +114,14 @@ fun provideTypography(): Typography {
                 }
             ),
             caption = it.caption.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color(0XCCFFFFFF)
                 } else {
                     Color(0XFF1D2238)
                 }
             ),
             overline = it.overline.copy(
-                color = if (isDarkTheme()) {
+                color = if (isDarkTheme) {
                     Color(0XCCFFFFFF)
                 } else {
                     Color(0XFF1D2238)
@@ -147,9 +153,9 @@ private fun isDarkTheme(): Boolean {
 }
 
 @Composable
-fun provideColor(): Colors {
+fun provideColor(isDarkTheme: Boolean): Colors {
     val primary = Color(0xFF1C68F3)
-    return if (isDarkTheme()) {
+    return if (isDarkTheme) {
         darkColors(
             primary = primary,
             secondary = primary,
