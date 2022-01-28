be4db44d14f7b13befbc00b4f824acd7dfa75191
seiko <seiko_des@outlook.com>
2022-01-28 14:51:28 +0800

    update room sql migration

--- wallet/src/main/java/com/dimension/maskbook/wallet/db/RoomMigrations.kt
+++ wallet/src/main/java/com/dimension/maskbook/wallet/db/RoomMigrations.kt
@@ -6,7 +6,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 object RoomMigrations {
     val MIGRATION_6_7 get() = object : Migration(6, 7) {
         override fun migrate(database: SupportSQLiteDatabase) {
-            database.execSQL("ALTER TABLE DbSendHistory ADD COLUMN `name` STRING DEFAULT '' NOT NULL")
+            database.execSQL("ALTER TABLE DbSendHistory ADD COLUMN `name` TEXT DEFAULT '' NOT NULL")
         }
     }
 }
