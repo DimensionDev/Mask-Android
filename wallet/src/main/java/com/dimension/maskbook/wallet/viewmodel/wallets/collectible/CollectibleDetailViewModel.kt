cc4a66ca22594249e25d15a79b984982913bcfac
seiko <seiko_des@outlook.com>
2022-01-29 12:08:46 +0800

    apply spotless

--- wallet/src/main/java/com/dimension/maskbook/wallet/viewmodel/wallets/collectible/CollectibleDetailViewModel.kt
+++ wallet/src/main/java/com/dimension/maskbook/wallet/viewmodel/wallets/collectible/CollectibleDetailViewModel.kt
@@ -1,22 +1,22 @@
 /*
  *  Mask-Android
  *
- *  Copyright (C) DimensionDev and Contributors
- * 
+ *  Copyright (C) 2022  DimensionDev and Contributors
+ *
  *  This file is part of Mask-Android.
- * 
+ *
  *  Mask-Android is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
+ *  it under the terms of the GNU Affero General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  Mask-Android is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- * 
- *  You should have received a copy of the GNU General Public License
- *  along with Mask-Android. If not, see <http://www.gnu.org/licenses/>.
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Mask-Android.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.dimension.maskbook.wallet.viewmodel.wallets.collectible
 
