b212582d5ab7a951e5294c3b3ab73f875a25b6cd
Tlaster <tlaster@outlook.com>
2022-02-11 16:52:54 +0800

    upgrade mask version

--- .github/build_maskbook.sh
+++ .github/build_maskbook.sh
@@ -2,7 +2,7 @@ mkdir build
 cd build
 git clone https://github.com/DimensionDev/Maskbook.git
 cd Maskbook
-git reset --hard 0f44eb2702b5a4adbba8d00c68b3bde03f246e73
+git reset --hard 493f5455d8b5dee475ffd7510518a90a9f45aaba
 pnpm install
 npx gulp build-ci
 mkdir -p ../../app/src/main/assets/web_extensions/Maskbook
