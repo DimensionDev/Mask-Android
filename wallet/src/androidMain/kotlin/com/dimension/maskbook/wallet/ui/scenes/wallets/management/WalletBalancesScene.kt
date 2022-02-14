75dff14973e8461694cb046a5470757d84ea31f3
seiko <seiko_des@outlook.com>
2022-02-14 20:44:43 +0800

    apply spotless

--- wallet/src/androidMain/kotlin/com/dimension/maskbook/wallet/ui/scenes/wallets/management/WalletBalancesScene.kt
+++ wallet/src/androidMain/kotlin/com/dimension/maskbook/wallet/ui/scenes/wallets/management/WalletBalancesScene.kt
@@ -48,9 +48,6 @@ import androidx.compose.material.ScrollableTabRow
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
