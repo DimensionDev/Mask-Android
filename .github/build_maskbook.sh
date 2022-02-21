0b3655e5f2f6055c361a1bdd4d428b92a78e492c
Tlaster <tlaster@outlook.com>
2022-02-21 14:45:29 +0800

    update mask source

--- .github/build_maskbook.sh
+++ .github/build_maskbook.sh
@@ -2,7 +2,7 @@ mkdir build
 cd build
 git clone https://github.com/DimensionDev/Maskbook.git
 cd Maskbook
-git reset --hard 215b391508549d24f97913fcac30c99f0da6f97e
+git reset --hard c6f9b512d29fcb1d61d8122aaff544bbd8d0d3ef
 pnpm install
 npx gulp build-ci
 mkdir -p ../../app/src/main/assets/web_extensions/Maskbook
