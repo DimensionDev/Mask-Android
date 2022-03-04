c63fa67090afbe6f9420447ca16d629ff434007b
Tlaster <tlaster@outlook.com>
2022-03-04 16:44:08 +0800

    upgrade dependency

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -19,15 +19,15 @@ object Versions {
         val java = JavaVersion.VERSION_11
     }
 
-    const val ksp = "${Kotlin.lang}-1.0.2"
+    const val ksp = "${Kotlin.lang}-1.0.4"
     const val agp = "7.1.0"
-    const val spotless = "6.2.0"
+    const val spotless = "6.3.0"
     const val ktlint = "0.43.2"
-    const val compose_jb = "1.1.0-alpha02"
-    const val compose = "1.1.0-rc03"
-    const val accompanist = "0.22.1-rc"
-    const val navigation = "2.4.0"
-    const val lifecycle = "2.4.0"
+    const val compose_jb = "1.1.0"
+    const val compose = "1.1.0"
+    const val accompanist = "0.23.0"
+    const val navigation = "2.4.1"
+    const val lifecycle = "2.4.1"
     const val koin = "3.1.4"
     const val coil = "1.4.0"
     const val datastore = "1.0.0"
@@ -38,10 +38,10 @@ object Versions {
     const val kotlinpoet = "1.10.2"
     const val material = "1.5.0"
     const val khexExtension = "1.1.2"
-    const val moshi = "1.8.0"
+    const val moshi = "1.13.0"
     const val walletConnectV1 = "0.9.7"
     const val zxing = "4.3.0"
-    const val maskWalletCore = "0.4.0"
+    const val maskWalletCore = "0.4.1"
     const val jodaTime = "2.10.13"
     object Firebase {
         object Plugin {
@@ -56,14 +56,14 @@ object Versions {
     object Androidx{
         const val core = "1.7.0"
         const val appcompat = "1.4.1"
-        const val room = "2.4.1"
+        const val room = "2.4.2"
         const val paging = "3.1.0"
         const val pagingCompose = "1.0.0-alpha14"
         const val livedata = "1.0.5"
         const val annotation = "1.3.0"
         const val activityCompose = "1.4.0"
         const val constraintlayout = "2.1.2"
-        const val preference = "1.1.1"
+        const val preference = "1.2.0"
         const val biometric = "1.2.0-alpha04"
     }
 }
