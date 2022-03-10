0bcc6eb1e2b81b032e7f95b2f8f6795ad79c33eb
huixing <wanghuixing24@gmail.com>
2022-03-10 21:35:06 +0800

    autoSize text

--- common/src/androidMain/kotlin/com/dimension/maskbook/common/ui/widget/SinglelineText.kt
+++ common/src/androidMain/kotlin/com/dimension/maskbook/common/ui/widget/SinglelineText.kt
@@ -20,11 +20,21 @@
  */
 package com.dimension.maskbook.common.ui.widget
 
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.BoxWithConstraintsScope
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.foundation.text.InternalFoundationTextApi
+import androidx.compose.foundation.text.TextDelegate
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFontLoader
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -34,13 +44,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.min
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SinglelineText(
     text: String,
     modifier: Modifier = Modifier,
     color: Color = Color.Unspecified,
-    fontSize: TextUnit = TextUnit.Unspecified,
+    suggestedFontSizes: List<TextUnit> = emptyList(),
     fontStyle: FontStyle? = null,
     fontWeight: FontWeight? = null,
     fontFamily: FontFamily? = null,
@@ -48,27 +60,117 @@ fun SinglelineText(
     textDecoration: TextDecoration? = null,
     textAlign: TextAlign? = null,
     lineHeight: TextUnit = TextUnit.Unspecified,
-    overflow: TextOverflow = TextOverflow.Ellipsis,
+    overflow: TextOverflow = TextOverflow.Clip,
     softWrap: Boolean = true,
+    maxLines: Int = 1,
     onTextLayout: (TextLayoutResult) -> Unit = {},
-    style: TextStyle = LocalTextStyle.current
+    style: TextStyle = LocalTextStyle.current,
 ) {
-    Text(
-        text = text,
-        modifier = modifier,
-        color = color,
-        fontSize = fontSize,
-        fontStyle = fontStyle,
-        fontWeight = fontWeight,
-        fontFamily = fontFamily,
-        letterSpacing = letterSpacing,
-        textDecoration = textDecoration,
-        textAlign = textAlign,
-        lineHeight = lineHeight,
-        overflow = overflow,
-        softWrap = softWrap,
-        maxLines = 1,
-        onTextLayout = onTextLayout,
-        style = style
+    AutoSizeText(
+        AnnotatedString(text),
+        modifier,
+        color,
+        suggestedFontSizes,
+        fontStyle,
+        fontWeight,
+        fontFamily,
+        letterSpacing,
+        textDecoration,
+        textAlign,
+        lineHeight,
+        overflow,
+        softWrap,
+        maxLines,
+        emptyMap(),
+        onTextLayout,
+        style,
     )
+}
+
+@Composable
+fun AutoSizeText(
+    text: AnnotatedString,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    suggestedFontSizes: List<TextUnit> = emptyList(),
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    inlineContent: Map<String, InlineTextContent> = mapOf(),
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = LocalTextStyle.current,
+) {
+    BoxWithConstraints(
+        modifier = modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        var combinedTextStyle = (LocalTextStyle.current + style).copy(
+            fontSize = min(maxWidth, maxHeight).value.sp
+        )
+
+        val fontSizes = suggestedFontSizes.ifEmpty {
+            MutableList(combinedTextStyle.fontSize.value.toInt()) {
+                (combinedTextStyle.fontSize.value - it).sp
+            }
+        }
+
+        var currentFontIndex = 0
+
+        while (shouldShrink(text, combinedTextStyle, maxLines)) {
+            combinedTextStyle =
+                combinedTextStyle.copy(fontSize = fontSizes[++currentFontIndex])
+        }
+
+        Text(
+            text,
+            Modifier,
+            color,
+            TextUnit.Unspecified,
+            fontStyle,
+            fontWeight,
+            fontFamily,
+            letterSpacing,
+            textDecoration,
+            textAlign,
+            lineHeight,
+            overflow,
+            softWrap,
+            maxLines,
+            inlineContent,
+            onTextLayout,
+            combinedTextStyle,
+        )
+    }
+}
+
+@OptIn(InternalFoundationTextApi::class)
+@Composable
+private fun BoxWithConstraintsScope.shouldShrink(
+    text: AnnotatedString,
+    textStyle: TextStyle,
+    maxLines: Int
+): Boolean {
+    val textDelegate = TextDelegate(
+        text,
+        textStyle,
+        maxLines,
+        true,
+        TextOverflow.Clip,
+        LocalDensity.current,
+        LocalFontLoader.current,
+    )
+
+    val textLayoutResult = textDelegate.layout(
+        constraints,
+        LocalLayoutDirection.current,
+    )
+
+    return textLayoutResult.hasVisualOverflow
 }
