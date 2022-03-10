42555f7dafdac3439bc96ea126650d8ab7c4c797
seiko <seiko_des@outlook.com>
2022-03-10 20:34:08 +0800

    create persona db

---
+++ persona/src/androidMain/kotlin/com/dimension/maskbook/persona/db/model/DbPersonaRecord.kt
@@ -0,0 +1,42 @@
+/*
+ *  Mask-Android
+ *
+ *  Copyright (C) 2022  DimensionDev and Contributors
+ *
+ *  This file is part of Mask-Android.
+ *
+ *  Mask-Android is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Mask-Android is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Mask-Android.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.dimension.maskbook.persona.db.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@kotlinx.serialization.Serializable
+@Entity
+data class DbPersonaRecord(
+    @PrimaryKey val identifier: String,
+    val mnemonic: String? = null,
+    val path: String? = null,
+    val withPassword: String? = null,
+    val publicKey: String,
+    val privateKey: String? = null,
+    val localKey: String? = null,
+    val nickname: String? = null,
+    val createAt: Long = 0,
+    val updateAt: Long = 0,
+    val hasLogout: Boolean = false,
+    val initialized: Boolean = false,
+    val avatar: String? = null,
+)
---
+++ persona/src/androidMain/kotlin/com/dimension/maskbook/persona/db/model/DbPostRecord.kt
@@ -0,0 +1,39 @@
+/*
+ *  Mask-Android
+ *
+ *  Copyright (C) 2022  DimensionDev and Contributors
+ *
+ *  This file is part of Mask-Android.
+ *
+ *  Mask-Android is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Mask-Android is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Mask-Android.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.dimension.maskbook.persona.db.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.serialization.json.JsonObject
+
+@Entity
+data class DbPostRecord(
+    @PrimaryKey val identifier: String,
+    val encryptBy: String?,
+    val postNetwork: String,
+    val postUserId: String,
+    val postCryptoKeyRaw: JsonObject?,
+    val url: String?,
+    val summary: String?,
+    val recipientsRaw: JsonObject?,
+    val interestedMetaRaw: JsonObject?,
+    val foundAt: Long,
+)
