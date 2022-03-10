4bc2d8cd0985971c40f092a6a13390d83b00c321
Tlaster <tlaster@outlook.com>
2022-03-10 20:05:41 +0800

    update mask upstream

--- .github/build_maskbook.sh
+++ .github/build_maskbook.sh
@@ -2,7 +2,7 @@ mkdir build
 cd build
 git clone https://github.com/DimensionDev/Maskbook.git
 cd Maskbook
-git reset --hard fee68cb4d58ab3b6417a17a73dc4d666463e1841
+git reset --hard 26da062c6b29d853000a589612ca4032e9451c41
 pnpm install
 npx gulp build-ci
 mkdir -p ../../extension/src/androidMain/assets/web_extensions/Maskbook
