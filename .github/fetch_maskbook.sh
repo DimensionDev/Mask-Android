4563f36a5e5c2e045d1a5ae7879f280dca2bc2c6
seiko <seiko_des@outlook.com>
2022-03-15 13:50:38 +0800

    fix ci build

--- .github/fetch_maskbook.sh
+++ .github/fetch_maskbook.sh
@@ -1,3 +1,3 @@
-wget -O ./Maskbook.zip https://51420-178806582-gh.circle-artifacts.com/0/MaskNetwork.gecko.zip --header "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3829.0 Safari/537.36 Edg/77.0.197.1"
+wget -O ./Maskbook.zip https://github.com/DimensionDev/Maskbook/releases/download/v2.5.2/MaskNetwork.gecko.zip --header "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3829.0 Safari/537.36 Edg/77.0.197.1"
 mkdir -p ./extension/src/androidMain/assets/web_extensions/Maskbook
 unzip -o ./Maskbook.zip -d ./extension/src/androidMain/assets/web_extensions/Maskbook
