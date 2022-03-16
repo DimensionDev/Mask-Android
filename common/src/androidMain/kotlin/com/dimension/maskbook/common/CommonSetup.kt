d48cc51966bb50b390ab25af55da35fe1cb575a0
seiko <seiko_des@outlook.com>
2022-03-16 10:43:29 +0800

    update io coroutineScope

--- common/src/androidMain/kotlin/com/dimension/maskbook/common/CommonSetup.kt
+++ common/src/androidMain/kotlin/com/dimension/maskbook/common/CommonSetup.kt
@@ -23,9 +23,11 @@ package com.dimension.maskbook.common
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import com.dimension.maskbook.common.util.BiometricAuthenticator
+import com.dimension.maskbook.common.util.coroutineExceptionHandler
 import com.dimension.maskbook.common.viewmodel.BiometricEnableViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
@@ -36,7 +38,7 @@ object CommonSetup : ModuleSetup {
 
     override fun dependencyInject() = module {
         single(named(IoScopeName)) {
-            CoroutineScope(Dispatchers.IO)
+            CoroutineScope(SupervisorJob() + Dispatchers.IO + coroutineExceptionHandler)
         }
         single {
             BiometricAuthenticator()
