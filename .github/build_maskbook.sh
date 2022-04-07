0df62e2011ea992b834f4ed91c80bf9628c2e486
seiko <seiko_des@outlook.com>
2022-04-07 18:25:30 +0800

    update maskbook

--- .github/build_maskbook.sh
+++ .github/build_maskbook.sh
@@ -2,7 +2,7 @@ mkdir build
 cd build
 git clone https://github.com/DimensionDev/Maskbook.git
 cd Maskbook
-git reset --hard ece338555828d3b55d6fe08609e9173146dd7c48
+git reset --hard 64e16929fd01fa331bec529f6b4628e5232cf228
 pnpm install
 npx gulp build-ci
 mkdir -p ../../extension/src/androidMain/assets/web_extensions/Maskbook
--- .github/fetch_maskbook.sh
+++ .github/fetch_maskbook.sh
@@ -1,4 +1,4 @@
-wget -O ./Maskbook.zip https://dl.circleci.com/private/output/job/7d79bb64-a663-49f1-acd5-670d5e0f0461/artifacts/0/MaskNetwork.gecko.zip --header "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3829.0 Safari/537.36 Edg/77.0.197.1"
+wget -O ./Maskbook.zip https://output.circle-artifacts.com/output/job/1d30e992-155b-4f0e-8b64-aa649d5784f0/artifacts/0/MaskNetwork.gecko.zip --header "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3829.0 Safari/537.36 Edg/77.0.197.1"
 mkdir -p ./extension/src/androidMain/assets/web_extensions/Maskbook
 unzip -o ./Maskbook.zip -d ./extension/src/androidMain/assets/web_extensions/Maskbook
 rm ./Maskbook.zip
