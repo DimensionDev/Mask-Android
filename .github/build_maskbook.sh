7a39a118790bc970a433ce4661afce5fff47da2b
seiko <seiko_des@outlook.com>
2022-04-13 15:49:25 +0800

    update maskbook

--- .github/build_maskbook.sh
+++ .github/build_maskbook.sh
@@ -2,7 +2,7 @@ mkdir build
 cd build
 git clone https://github.com/DimensionDev/Maskbook.git
 cd Maskbook
-git reset --hard 64e16929fd01fa331bec529f6b4628e5232cf228
+git reset --hard 0865bf42d992f44960660fe226d2d41fb850917b
 pnpm install
 npx gulp build-ci
 mkdir -p ../../extension/src/androidMain/assets/web_extensions/Maskbook
