47f4161b0ac1562f4437027f9fb023188d9f62f2
seiko <seiko_des@outlook.com>
2022-04-14 12:47:21 +0800

    apply spotless

--- wallet/src/androidMain/kotlin/com/dimension/maskbook/wallet/ui/scenes/wallets/management/WalletSwitchModal.kt
+++ wallet/src/androidMain/kotlin/com/dimension/maskbook/wallet/ui/scenes/wallets/management/WalletSwitchModal.kt
@@ -54,7 +54,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
