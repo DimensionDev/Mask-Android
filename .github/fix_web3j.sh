7faf8421abe8b0564448c1c1a9f25f5f5626b447
seiko <seiko_des@outlook.com>
2022-04-21 10:13:30 +0800

    fix ubuntu sed sh

--- .github/fix_web3j.sh
+++ .github/fix_web3j.sh
@@ -1,1 +1,6 @@
-sed -i "" "s/this\.fetch = false;//" "$1/src/androidMain/assets/web_extensions/Maskbook/js/npm-ns.solana.web3.js.js"
+sed_option=
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed_option="-i ''"
+fi
+
+sed "$sed_option" "s/this\.fetch = false;//" "$1/src/androidMain/assets/web_extensions/Maskbook/js/npm-ns.solana.web3.js.js"
