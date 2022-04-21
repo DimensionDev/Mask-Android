55ee1a90be08792402973facb9f78be6e9d5e33d
seiko <seiko_des@outlook.com>
2022-04-21 10:23:03 +0800

    fix build

--- .github/fix_web3j.sh
+++ .github/fix_web3j.sh
@@ -1,6 +1,6 @@
 sed_option=
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed_option="-i ''"
+  sed_option="''"
 fi
 
-sed "$sed_option" "s/this\.fetch = false;//" "$1/src/androidMain/assets/web_extensions/Maskbook/js/npm-ns.solana.web3.js.js"
+sed -i "$sed_option" "s/this\.fetch = false;//" "$1/src/androidMain/assets/web_extensions/Maskbook/js/npm-ns.solana.web3.js.js"
