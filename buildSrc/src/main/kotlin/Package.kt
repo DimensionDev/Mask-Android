ddf7b4913eec350b2ef1b442a359c95c3fdad166
Tlaster <tlaster@outlook.com>
2022-06-21 14:57:06 +0800

    version 2.9.0-beta02-uifix

--- buildSrc/src/main/kotlin/Package.kt
+++ buildSrc/src/main/kotlin/Package.kt
@@ -10,7 +10,7 @@ object Package {
         const val main = "2"
         const val mirror = "9"
         const val patch = "0"
-        const val revision = "beta02"
+        const val revision = "beta02-uifix"
         const val build = 71
     }
 }
